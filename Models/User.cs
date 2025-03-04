f52a63ddd7c390d597400ad3bed607041745c721
hungleip9@gmail.com <hungleip9@gmail.com>
2025-03-04 11:31:10 +0700

    updated

---
+++ Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nuxt_shop.Models;
+
+public partial class User
+{
+    public string UserName { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int PhoneNumber { get; set; }
+
+    public string? Address { get; set; }
+
+    public int Id { get; set; }
+
+    public string? Status { get; set; }
+
+    public string? Photo { get; set; }
+
+    public bool IsLocked { get; set; }
+
+    public string? IdNumber { get; set; }
+
+    public DateOnly? DateOfBirth { get; set; }
+
+    public string? Otp { get; set; }
+
+    public DateTime? OtpExpired { get; set; }
+}
